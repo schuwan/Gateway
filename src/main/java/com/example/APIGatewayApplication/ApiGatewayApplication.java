cdbd1cf2a02a206dc5dd27d44f9df1f4b3782c4a
John Crofford <59934726+Johngh17@users.noreply.github.com>
2021-11-01 16:36:11 -0500

    Initial Commit

---
+++ src/main/java/com/example/APIGatewayApplication/ApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.example.APIGatewayApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/APIGatewayApplication/gateway/SpringCloudConfig.java
@@ -0,0 +1,22 @@
+package com.example.APIGatewayApplication.gateway;
+
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringCloudConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r->r.path("/users/**")
+                        .uri("lb://USER-SERVICE"))
+                .route(r->r.path("/posts/**")
+                        .uri("lb://POST-SERVICE"))
+                .build();
+
+    }
+}
